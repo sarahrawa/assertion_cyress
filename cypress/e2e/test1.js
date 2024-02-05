4a9da2b60714b34a0bfc944d8126ef1e808e588d
sarahrawa <saraialrawashdeh@gmail.com>
2024-02-05 02:41:22 +0200

    first commit

--- cypress/e2e/test1.js
+++ cypress/e2e/test1.js
@@ -20,7 +20,46 @@ describe('welcome to my project', () => {
         //positive senario
         cy.get('.mb40 > :nth-child(3)').should('have.text',"Your enquiry has been successfully sent to the store owner!")
         //false senario
-        cy.get('.mb40 > :nth-child(3)').should('have.text',"Yours enquiry has been successfully sent to the store owner!")
+       // cy.get('.mb40 > :nth-child(3)').should('have.text',"Yours enquiry has been successfully sent to the store owner!")
+        //property assertion
+        //methode1
+        //cy.get('a[title="Continue"]').should("have.property",'title')
+        //methode2
+        cy.document('a[title="Continue"]').should("have.property",'title')
+
+        //assertion of title
+        cy.document().should('have.property','title').and('eq','Contact Us')
+
+        //assertion of title
+        cy.title().should('include','Contact')
+        cy.title().should('eq','Contact Us')
+        //assertion Url
+        cy.location('href').should('include','success')
+        cy.location().its('href').should('include','success')
+        cy.url().should('include','success')
+
+        //return to home page
+        cy.get('i[title="Home"]').click()
+        //select items and print message after click
+        cy.get('.prdocutname').contains('BeneFit Girl Meets Pearl').click().then(function(elemnt){
+
+            console.log(elemnt.text()+" has been added")
+        })
+        //return to home page and select shipping
+        cy.get('i[title="Home"]').click()
+        cy.get('.info_links_footer').contains('Shipping').click().then(function(element1){
+            console.log(element1.text()+" has been added")
+        })
+        //return to home page and select contact us
+        cy.get('i[title="Home"]').click()
+        cy.get('.info_links_footer').contains('Contact Us').click().then(function(element2){
+            console.log(element2.text()+" has been added")
+        })
+        //return to home page and select Site Map
+        cy.get('i[title="Home"]').click()
+        cy.get('.info_links_footer').contains('Site Map').click().then(function(element3){
+            console.log(element3.text()+" has been added")
+        })
 
     });
     
